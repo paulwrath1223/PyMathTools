452383027857ebb8f15103a20058c6d11c5ceeee
paulwrath1223 <paul@fornage.net>
2021-09-15 12:48:20 -0700

    updated user prompts

--- equation solver.py
+++ equation solver.py
@@ -4,15 +4,18 @@ from sympy.solvers import solve
 init_printing(use_unicode=True)
 
 
-a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = symbols("a b c d e f g h i j k l m n o p q r s t u v w x y z")
-originalEq = input("enter equation with variables represented by lowercase variables.\n")
-originalEq = "("+originalEq.replace("=",")-(")+")"
+a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = symbols\
+    ("a b c d e f g h i j k l m n o p q r s t u v w x y z")
+originalEq = input\
+    ("enter equation with variables represented by lowercase variables. You MUST add \"*\", you cannot do \"2x\"\n")
+originalEq = "("+originalEq.replace("=", ")-(")+")".replace("^", "**")
 varLetter = input("What variable should be solved for? ")
 
 expression = sympify(originalEq)
 varToSolveFor = sympify(varLetter)
 
 answer = (solve(expression, varToSolveFor))
-answerFormatted = (str(answer)).replace("[","").replace("]","")
+answerFormatted = (str(answer)).replace("[", "").replace("]", "")
 print(f"Solution:\n{varToSolveFor} = {answerFormatted}")
+
 
