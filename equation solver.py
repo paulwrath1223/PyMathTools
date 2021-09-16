b57f32d93962d85dbd54f68f15d28122c72a45b6
paulwrath1223 <paul@fornage.net>
2021-09-16 13:24:21 -0700

    updated user prompts

--- equation solver.py
+++ equation solver.py
@@ -6,23 +6,30 @@ init_printing(use_unicode=True)
 
 a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = symbols\
     ("a b c d e f g h i j k l m n o p q r s t u v w x y z")
-originalEq = input\
-    ("enter equation with variables represented by lowercase variables. You MUST add \"*\", you cannot do \"2x\"\n")
-originalEq = "("+originalEq.replace("=", ")-(")+")".replace("^", "**")
-varLetter = input("What variable should be solved for?\n")
+while True:
+    originalEq = input\
+        ("enter equation with variables represented by lowercase variables. You MUST add \"*\", you cannot do \"2x\"\n")
 
-expression = sympify(originalEq)
-varToSolveFor = sympify(varLetter)
+    originalEq = "("+originalEq.replace("=", ")-(")+")".replace("^", "**")
+    varLetter = input("What variable should be solved for?\n")
 
-answer = sympify(solve(expression, varToSolveFor))
-answerFormatted = (str(answer)).replace("[", "").replace("]", "").replace("**","^")
-print(f"Solution:\n{varToSolveFor} = {answerFormatted}")
+    expression = sympify(originalEq)
+    varToSolveFor = sympify(varLetter)
 
-# NOT WORKING, Try subbing before the solution and resolving
+    answer = sympify(solve(expression, varToSolveFor))
+    answerFormatted = (str(answer)).replace("[", "").replace("]", "").replace("**","^")
+    print(f"Solution:\n{varToSolveFor} = {answerFormatted}")
 
-# subLetter = sympify(input("What variable should be substituted?\n"))
-# while(subLetter != ""):
-#     answer = sympify(answer).subs(subLetter,input("What should it be replaced with?\n"))
-#     answerFormatted = (str(answer)).replace("[", "").replace("]", "")
-#     print(f"Solution:\n{varToSolveFor} = {answerFormatted}")
-#     subLetter = sympify(input("Next variable to be substituted?\n"))
+    # NOT WORKING, Try subbing before the solution and resolving
+
+    subLetter = sympify(input("What variable should be substituted? (none to skip)\n"))
+    while(subLetter != ""):
+
+        for i in range(len(answer)):
+            x = answer[i]
+            x = x.subs(subLetter, input("What should it be replaced with?\n"))
+            answer[i] = x
+
+        answerFormatted = (str(answer)).replace("[", "").replace("]", "")
+        print(f"Solution:\n{varToSolveFor} = {answerFormatted}")
+        subLetter = sympify(input("Next variable to be substituted?\n"))
