60dccd3fe4f5c84c3d8e1f3458494f8614c1261b
paulwrath1223 <paul@fornage.net>
2021-09-16 10:31:23 -0700

    updated user prompts

--- equation solver.py
+++ equation solver.py
@@ -9,13 +9,20 @@ a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = s
 originalEq = input\
     ("enter equation with variables represented by lowercase variables. You MUST add \"*\", you cannot do \"2x\"\n")
 originalEq = "("+originalEq.replace("=", ")-(")+")".replace("^", "**")
-varLetter = input("What variable should be solved for? ")
+varLetter = input("What variable should be solved for?\n")
 
 expression = sympify(originalEq)
 varToSolveFor = sympify(varLetter)
 
-answer = (solve(expression, varToSolveFor))
+answer = sympify(solve(expression, varToSolveFor))
 answerFormatted = (str(answer)).replace("[", "").replace("]", "")
 print(f"Solution:\n{varToSolveFor} = {answerFormatted}")
 
+# NOT WORKING, Try subbing before the solution and resolving
 
+# subLetter = sympify(input("What variable should be substituted?\n"))
+# while(subLetter != ""):
+#     answer = sympify(answer).subs(subLetter,input("What should it be replaced with?\n"))
+#     answerFormatted = (str(answer)).replace("[", "").replace("]", "")
+#     print(f"Solution:\n{varToSolveFor} = {answerFormatted}")
+#     subLetter = sympify(input("Next variable to be substituted?\n"))
