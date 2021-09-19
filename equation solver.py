6697c3562a6f8b273c1f185eb9a77b4ce7a20863
paulwrath1223 <paul@fornage.net>
2021-09-19 12:15:01 -0700

    added multiplify function from asymptote_finder

--- equation solver.py
+++ equation solver.py
@@ -6,9 +6,27 @@ init_printing(use_unicode=True)  # Don't even know what this does
 
 a, b, c, d, f, g, h, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = symbols(
     "a b c d f g h j k l m n o p q r s t u v w x y z")  # declare a mathematical symbol for all letters except e and i
+
+
+def multiplify(express):  # adds '*' in between ")(", "
+    express_list = list(str(express))
+    out = ""
+    counter = 0
+    for counter in range(len(express_list)-1):
+        char = express_list[counter]
+        out += char
+        if char.isnumeric() or char == "x" or char == ")":
+            next_char = express_list[counter+1]
+            if next_char == "x" or next_char == "(":
+                out += "*"
+    out += express_list[counter+1]
+    return out
+    # sample: "2(x-1)" -> "2*(x-1)"
+
+
 while True:
-    originalEq = input(
-        "enter equation with variables represented by lowercase variables. You MUST add \"*\", you cannot do \"2x\"\n")
+    originalEq = multiplify(input(
+        "enter equation with variables represented by lowercase letters. "))
 
     originalEq = "("+originalEq.replace(  # convert equation to an expression equal to 0
         "=", ")-(")+")".replace("^", "**")
@@ -24,7 +42,7 @@ while True:
     subLetter = input("What variable should be substituted? (\"none\" to skip)\n")
     while subLetter != "none":
         subLetter = sympify(subLetter)
-        replacement = input("What should it be replaced with?\n")
+        replacement = multiplify(input("What should it be replaced with?\n"))
         for i in range(len(answer)):  # substitute all occurrences of subLetter within the expression with replacement
             x = answer[i]
             x = x.subs(subLetter, replacement)
--- grapher.py
+++ grapher.py
@@ -1,7 +1,25 @@
 
 from sympy import *
+
+
+def multiplify(express):  # adds '*' in between ")(", "
+    express_list = list(str(express))
+    out = ""
+    counter = 0
+    for counter in range(len(express_list)-1):
+        char = express_list[counter]
+        out += char
+        if char.isnumeric() or char == "x" or char == ")":
+            next_char = express_list[counter+1]
+            if next_char == "x" or next_char == "(":
+                out += "*"
+    out += express_list[counter+1]
+    return out
+    # sample: "2(x-1)" -> "2*(x-1)"
+
+
 x = symbols('x')
-expr = sympify(input("expression: \ny = ").replace("^", "**"))
+expr = sympify(multiplify(input("expression: \ny = ").replace("^", "**")))
 rangeX = (input("range of x values to render: ex. \"a, b\"\n")).split(",")
 if rangeX[0] > rangeX[1]:
     temp = rangeX[0]
